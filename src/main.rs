a189e6204c9fc2641cec47909b840a35ca6cfd79
Joel Willoughby <jawilloughby68@gmail.com>
2019-01-15 21:09:00 -0800

    Initial commit with some cool stuff

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess stuff!");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    println!("The secret is {}", secret_number);
+
+    println!("Please input stuff.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+    println!("You guessed {}", guess);
+
+    match guess.cmp(&secret_number) {
+    	Ordering::Less => println!("Too Small"),
+    	Ordering::Greater => println!("Too Big"),
+    	Ordering::Equal => println!("Yep...."),
+    }
+
+}
